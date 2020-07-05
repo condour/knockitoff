04492f0d935b72e1db519c3f4026e132d17bd977
Michael Condouris <condour@gmail.com>
2020-07-05 17:52:42 -0400

    before adding LCD

--- knockitoff.py
+++ knockitoff.py
@@ -14,11 +14,11 @@
     note: with 2048 samples per chunk, I'm getting 20FPS
     when also running the spectrum, its about 15FPS
 """
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 import pyaudio
-from pyqtgraph.Qt import QtGui, QtCore
-import pyqtgraph as pg
+#from pyqtgraph.Qt import QtGui, QtCore
+#import pyqtgraph as pg
 import struct
 from numpy import average, log10
 import math
@@ -26,7 +26,27 @@ import sys
 import time
 import RPi.GPIO as GPIO
 import time
+from collections import deque
+import os
+import paramiko
 
+from dotenv import load_dotenv
+load_dotenv()
+
+pw = os.environ.get("ROUTER_PW")
+
+# Defines the data bit that is transmitted preferentially in the shiftOut function.
+LSBFIRST = 1
+MSBFIRST = 2
+# define the pins for 74HC595
+dataPin   = 11      # DS Pin of 74HC595(Pin14)
+latchPin  = 13      # ST_CP Pin of 74HC595(Pin12)
+clockPin = 15       # CH_CP Pin of 74HC595(Pin11)
+greenPin = 16
+redPin = 18
+inTimeout = False
+timeoutStart = time.time()
+print("timeoutStart" + str(timeoutStart))
 def rms( data ):
     count = len(data)/2
     format = "%dh"%(count)
@@ -40,7 +60,10 @@ def rms( data ):
 def decibel(rms):
     decibel = 20 * log10(rms)
     return decibel
-    
+
+internetOff = "/usr/sbin/iptables -I FORWARD -s 192.168.1.18 -j DROP && /usr/sbin/iptables -I FORWARD -s 192.168.1.27 -j DROP"
+internetOn = "/usr/sbin/iptables -D FORWARD -s 192.168.1.18 -j DROP && /usr/sbin/iptables -D FORWARD -s 192.168.1.27 -j DROP"
+#internetOff = "ls -la"
 # shiftOut function, use bit serial transmission. 
 def shiftOut(dPin,cPin,order,val):
     for i in range(0,8):
@@ -56,8 +79,10 @@ def setup():
     GPIO.setup(dataPin, GPIO.OUT) # set pin to OUTPUT mode
     GPIO.setup(latchPin, GPIO.OUT)
     GPIO.setup(clockPin, GPIO.OUT)
-
-
+    GPIO.setup(redPin, GPIO.OUT)
+    GPIO.setup(greenPin, GPIO.OUT)
+    GPIO.output(greenPin, GPIO.HIGH)
+    
 # stream constants
 CHUNK = 1024 * 2
 FORMAT = pyaudio.paInt16
@@ -77,6 +102,18 @@ stream = p.open(
     frames_per_buffer=CHUNK,
 )
 
+def turnInternet(on):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect("192.168.1.1", username="root", password=pw)
+    if(on):
+        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(internetOn)
+    else:
+        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(internetOff)
+        print("turned internet off")
+        print(ssh_stdout)
+        print(ssh_stderr)
+        
 def scale(val, src, dst):
     """
     Scale the given value from the scale of src to the scale of dst.
@@ -84,27 +121,70 @@ def scale(val, src, dst):
     return ((val - src[0]) / (src[1]-src[0])) * (dst[1]-dst[0]) + dst[0]
 
 def loop():
+    mad = 0
+    global inTimeout, timeoutStart
+    q = deque(maxlen = 30)
     while True:
         data = stream.read(CHUNK, exception_on_overflow = False)
         #data_np = struct.unpack(str(2 * CHUNK) + 'B', data)
         dec = decibel(rms(data))
-        whichBar = int(round(scale(dec, [-70, -30],[1,7])))
-        print(whichBar)
-        # if(dec > -35):
-            # print("BOOM")
-        # elif(dec > -50):
-            # print("boom")
-        # else:
-            # print("...")
+        whichBar = max(min(int(round(scale(dec, [-70, -30],[7,0]))),7),0)
+        q.appendleft(7 - whichBar)
+        if(average(q) > 5):
+            GPIO.output(redPin, GPIO.HIGH)
+            mad = mad + 1
+            print("mad" + str(mad))
+            if(mad > 50):
+                # GO TO FAIL STATE
+                print("MAD " + str(mad))
+                if(not inTimeout):
+                    GPIO.output(greenPin, GPIO.LOW)
+                    timeoutStart = time.time()
+                    inTimeout = True
+                    print("turning off")
+                    turnInternet(False)
+                    print("turned off")
+        else:
+            if(mad <= 0):
+                GPIO.output(redPin, GPIO.LOW)
+            mad = max(0, mad - .25)
+            if(inTimeout):
+                secondsTil = 30 - int(round(time.time() - timeoutStart))
+                if(secondsTil % 2 == 1):
+                    GPIO.output(redPin, GPIO.HIGH)
+                else:
+                    GPIO.output(redPin, GPIO.LOW)
+                print(secondsTil)
+            if(mad == 0 and inTimeout and time.time() - timeoutStart > 30):
+                print("turning on")
+                turnInternet(True)
+                print("turned on")
+                GPIO.output(greenPin, GPIO.HIGH)
+                inTimeout = False
+                print("restored")
+
+        GPIO.output(latchPin,GPIO.LOW)  # Output low level to latchPin
+        shiftOut(dataPin,clockPin,LSBFIRST,0xFF << whichBar) # Send serial data to 74HC595
+        GPIO.output(latchPin,GPIO.HIGH)
         
 
 def destroy():
     stream.stop_stream()
     stream.close()
     p.terminate()
+    if(inTimeout):
+        print("should restore internet")
+        turnInternet(True)
+    print("should turn off lights")
+    shiftOut(dataPin,clockPin,MSBFIRST,0x00)
+    
     GPIO.cleanup()
 
+if __name__ == '__main__':  # Program entrance
+    print ('Program is starting...' )
+    setup() 
 try:
+    #shiftOut(dataPin, clockPin, LSBFIRST, 3)
     loop()  
 except KeyboardInterrupt:   # Press ctrl-c to end the program.
     destroy()  
